6930a8c072dbd34b53ec47dbcd87a9a746bc21e7
matthew <larochelle.matthew@gmail.com>
2023-05-22 17:36:52 -0700

    login logout page routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom'
-
+import { BrowserRouter, Route, Router, Routes, Navigate } from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext'
 // pages & components
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
@@ -7,6 +7,8 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 
 function App() {
+  const { user } = useAuthContext()
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -15,15 +17,15 @@ function App() {
           <Routes>
             <Route
               path='/'
-              element={<Home />}
+              element={user ? <Home /> : <Navigate to="/login" />}
               />
               <Route
               path='/login'
-              element={<Login />}
+              element={!user ? <Login /> : <Navigate to="/" />}
               />
               <Route
               path='/signup'
-              element={<Signup />}
+              element={!user ? <Signup /> : <Navigate to="/" />}
               />
           </Routes>
         </div>
