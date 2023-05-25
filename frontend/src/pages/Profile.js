9aafa501c29581a172db6e01f500d604ad7ba776
matthew <larochelle.matthew@gmail.com>
2023-05-24 19:46:41 -0700

    updates

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -4,6 +4,11 @@ const Profile = () => {
     return (
         <div className="profile">
             Hello Profile
+
+            Create a practice journal
+            Create a settings page - to edit the email/password
+            Create a max bpm spot
+            Create 3 secions - songs known, songs working, dream songs
         </div>
     )
 }
