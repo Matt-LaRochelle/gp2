d1a909824e3dee1664d2d459eac682c2f6d8cf4c
matthew <larochelle.matthew@gmail.com>
2023-05-24 19:47:06 -0700

    update 2

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -5,6 +5,7 @@ const Profile = () => {
         <div className="profile">
             Hello Profile
 
+            Before we do anything we need to set the color scheme better
             Create a practice journal
             Create a settings page - to edit the email/password
             Create a max bpm spot
