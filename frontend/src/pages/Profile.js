43c470068956bd31521332b88c1fb9b60010f514
matthew <larochelle.matthew@gmail.com>
2023-05-31 11:51:21 -0700

    add formatted date

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { usePracticesContext } from '../hooks/usePracticesContext'
 import { useAuthContext } from '../hooks/useAuthContext'
 import { useScoresContext } from '../hooks/useScores'
+import { format } from 'date-fns'
 
 // components
 import PracticeDetails from '../components/PracticeDetails'
@@ -53,10 +54,22 @@ const Profile = () => {
         
     }, [scoresDispatch, user])
 
+    let date = format(new Date(), 'EEEE, MMMM do, yyyy')
+    
+
+
     return (
         <div className="home">
-        <h2>Scores:</h2>
-        <p>Single notes: {scores[0].single}</p>
+        <div>
+            <h2>Scores:</h2>
+            <p>Single notes: {scores[0].single}</p>
+        </div>
+        <div>
+            Hello {user.email}
+        </div>
+        <h2>Practice Journal</h2>
+        <h2>Today is {date}</h2>
+        
             <div className="practices">
                 {practices && practices.map((practice) => (
                     <PracticeDetails key={practice._id} practice={practice} />
