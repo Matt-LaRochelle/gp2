b2d2732a0fe4109b5f7c780f2f4b269c76667688
matthew <larochelle.matthew@gmail.com>
2023-06-05 14:46:49 -0700

    Home done for now

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,35 +1,7 @@
-import { useEffect } from 'react'
-import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
-import { useAuthContext } from '../hooks/useAuthContext'
 import { useSetup } from '../hooks/useSetup'
 
-// components
-import WorkoutDetails from '../components/WorkoutDetails'
-import WorkoutForm from '../components/WorkoutForm'
-
 const Home = () => {
-    const {workouts, dispatch} = useWorkoutsContext()
-    const { user } = useAuthContext()
-    const {setup, error, isLoading} = useSetup()
-
-    useEffect(() => {
-        const fetchWorkouts = async () => {
-            const response = await fetch('/api/workouts', {
-                headers: {
-                    'Authorization': `Bearer ${user.token}`
-                }
-            })
-            const json = await response.json()
-
-            if (response.ok) {
-                dispatch({type: 'SET_WORKOUTS', payload: json})
-            }
-        }
-
-        if (user) {
-            fetchWorkouts()
-        }
-    }, [dispatch, user])
+    const {setup} = useSetup()
 
     const setScores = () => {
         console.log("let's go!")
@@ -37,13 +9,7 @@ const Home = () => {
     }
 
     return (
-        <div className="home">
-            <div className="workouts">
-                {workouts && workouts.map((workout) => (
-                    <WorkoutDetails key={workout._id} workout={workout} />
-                ))}
-            </div>
-            <WorkoutForm />
+        <div>
             <button onClick={setScores}>Set the scores</button>
         </div>
     )
