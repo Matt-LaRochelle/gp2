31bbe70a1170276f35b3521d5fca1fe789e65169
matthew <larochelle.matthew@gmail.com>
2023-06-06 14:11:29 -0700

    Final touches

--- frontend/src/pages/Eartraining.js
+++ frontend/src/pages/Eartraining.js
@@ -39,10 +39,10 @@ const Eartraining = () => {
                 <button id="5" onClick={toggle}>Level 5</button>
             </div>
             {display === "1" && <Single />}
-            {display === "2" && <p>Interval</p>}
-            {display === "3" && <p>Chord</p>}
-            {display === "4" && <p>Scale</p>}
-            {display === "5" && <p>Progression</p>}
+            {display === "2" && <p>Interval - coming soon...</p>}
+            {display === "3" && <p>Chord - coming soon...</p>}
+            {display === "4" && <p>Scale - coming soon...</p>}
+            {display === "5" && <p>Progression - coming soon...</p>}
         </div>
     )
 }
