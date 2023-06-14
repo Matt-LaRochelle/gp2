ee248c35618a6b235c1492372a40b42ca06ace31
matthew <larochelle.matthew@gmail.com>
2023-06-14 10:22:48 -0700

    add more logic to backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,6 +15,10 @@ const loginUser = async (req, res) => {
         // create a token
         const token = createToken(user._id)
 
+        // get extra user information for application
+        const fName = user.fName
+        const year = user.year
+
         res.status(200).json({email, fName, year, token})
     } catch (error) {
         res.status(400).json({error: error.message})
