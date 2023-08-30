5eb6813b50b5c4b1deaf07788ef0f16b493a94a1
matthew <larochelle.matthew@gmail.com>
2023-08-30 13:10:32 -0700

    key window minimize maximize correctly

--- frontend/src/components/singleKey/SingleKey.js
+++ frontend/src/components/singleKey/SingleKey.js
@@ -2,9 +2,26 @@ import sharps from '../Sharps.png'
 import flats from '../Flats.png'
 import './singleKey.css'
 import { useState, useEffect } from 'react'
+import { AiOutlineFullscreen } from 'react-icons/ai'
 
 const SKey = ({type}) => {
     const [sharpKey, setSharpKey] = useState(true)
+    const [isMaximized, setIsMaximized] = useState(false);
+
+    const toggleMaximize = () => {
+        setIsMaximized(!isMaximized);
+      };
+
+      const windowStyles = {
+        // Apply styles based on the maximize state
+        width: isMaximized ? '100%' : '300px',
+        height: isMaximized ? 'auto' : '200px',
+        position: isMaximized ? 'fixed' : 'absolute',
+        top: isMaximized ? '0' : '200px',
+        left: isMaximized ? '0' : '50%',
+        transform: isMaximized ? 'none' : 'translate(-50%, -50%)',
+        zIndex: isMaximized ? '999' : '0',
+      };
 
     // To do:
     // In here you can have a maximize/minimize window
@@ -21,12 +38,15 @@ const SKey = ({type}) => {
     }, [type])
 
     return (
-        <div className="singleKeyContainer">
-            {
-                sharpKey 
-                ? <img src={sharps} alt="notes on the guitar using naturals and sharps" />
-                : <img src={flats} alt="notes on the guitar using naturals and flats" />
-            }
+        <div>
+            <div className="window" style={windowStyles}>
+                {
+                    sharpKey 
+                    ? <img src={sharps} alt="notes on the guitar using naturals and sharps" />
+                    : <img src={flats} alt="notes on the guitar using naturals and flats" />
+                }
+                <AiOutlineFullscreen className='maximize' onClick={toggleMaximize} />
+            </div>
         </div>
     )
 }
