cb49b0ad1141a4ce4588b59806e5d6536bd80e30
matthew <larochelle.matthew@gmail.com>
2023-08-31 12:12:29 -0700

    Basic tuner built

--- frontend/src/pages/tuner/Tuner.js
+++ frontend/src/pages/tuner/Tuner.js
@@ -1,9 +1,28 @@
 import './tuner.css'
+import Navbar from '../../components/navbar/Navbar'
 
 const Tuner = () => {
     return (
-        <div>
-            TUNER HERE!?!?
+        <div className="tunerPageContainer">
+            <Navbar />
+            <div className="tunerContainer">
+            <div className='tunerTop'>
+                <h1>b</h1><h1>#</h1>
+            </div>
+            <div className='tunerMiddle'>
+                
+            </div>
+
+            <div className="tunerBottom">
+                <div className="tunerPegNamesLeft">
+                    <p>D</p><p>A</p><p>E</p>
+                </div>
+                <div className="tunerPegNamesRight">
+                    <p>G</p><p>B</p><p>E</p>
+                </div>
+                <img className="tunerHeadstock" src="https://static.vecteezy.com/system/resources/previews/009/383/457/original/guitar-head-clipart-design-illustration-free-png.png" />
+            </div>
+            </div>
         </div>
     )
 }
