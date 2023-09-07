e84f78d7b38e162c09cf96bf9ddba4bdea31c3d5
matthew <larochelle.matthew@gmail.com>
2023-09-07 09:59:29 -0700

    small edits, app is complete

--- frontend/src/components/singleKey/SingleKey.js
+++ frontend/src/components/singleKey/SingleKey.js
@@ -2,30 +2,9 @@ import sharps from '../Sharps.png'
 import flats from '../Flats.png'
 import './singleKey.css'
 import { useState, useEffect } from 'react'
-import { AiOutlineFullscreen, AiOutlineFullscreenExit } from 'react-icons/ai'
 
 const SKey = ({type}) => {
     const [sharpKey, setSharpKey] = useState(true)
-    const [isMaximized, setIsMaximized] = useState(false);
-
-    const toggleMaximize = () => {
-        setIsMaximized(!isMaximized);
-      };
-
-      const windowStyles = {
-        // Apply styles based on the maximize state
-        minWidth: isMaximized ? '100%' : '',
-        // minHeight: isMaximized ? 'auto' : '700px',
-        position: isMaximized ? 'fixed' : 'absolute',
-        top: isMaximized ? '0' : '150px',
-        left: isMaximized ? '0' : '50%',
-        transform: isMaximized ? 'none' : 'translateX(-50%)',
-        zIndex: isMaximized ? '999' : '0',
-      };
-
-    // To do:
-    // Still have a background fade color 
-    // but make sure you can click through it
 
     useEffect(() => {
         if (type) {
@@ -37,16 +16,11 @@ const SKey = ({type}) => {
 
     return (
         <div className="keyContainer">
-            <div className="window" style={windowStyles}>
+            <div className="window">
                 {
-                    sharpKey 
-                    ? <img src={sharps} alt="notes on the guitar using naturals and sharps" />
-                    : <img src={flats} alt="notes on the guitar using naturals and flats" />
-                }
-                {
-                    isMaximized
-                    ? <AiOutlineFullscreenExit className='minimize' onClick={toggleMaximize} />
-                    : <AiOutlineFullscreen className='maximize' onClick={toggleMaximize} />
+                sharpKey 
+                ? <img src={sharps} alt="notes on the guitar using sharps" />
+                : <img src={flats} alt="notes on the guitar using flats" />
                 }
             </div>
         </div>
